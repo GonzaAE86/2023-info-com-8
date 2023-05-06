f883afb58071041bb7d54f33978660af2d2a3dc7
Gonzalo <gonzalomongelos290@gmail.com>
2023-05-06 19:34:46 -0300

    Mi Tarea

---
+++ Ejercicio5.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingrese el primer número entero: "))
+num2 = int(input("Ingrese el segundo número entero: "))
+
+cociente = num1 // num2
+resto = num1 % num2
+
+print("El cociente de la división de", num1, "entre", num2, "es:", cociente)
+print("El resto de la división de", num1, "entre", num2, "es:", resto)
---
+++ Ejercicio9.py
@@ -0,0 +1,12 @@
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print("La suma de", num1, "y", num2, "es:", suma)
+print("La resta de", num1, "y", num2, "es:", resta)
+print("La multiplicación de", num1, "y", num2, "es:", multiplicacion)
+print("La división de", num1, "y", num2, "es:", division)
